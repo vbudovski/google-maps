e07bab63fb989b06015e6b6398e76e5031dd8d0b
Vitaly Budovski <vbudovski@gmail.com>
2024-04-05 12:25:25 +1100

    test: streetViewMetadata

--- src/test/streetViewApi.spec.ts
+++ src/test/streetViewApi.spec.ts
@@ -1,5 +1,7 @@
 import { describe, expect, test } from 'vitest';
-import { streetView } from '..';
+import { z } from 'zod';
+import { streetView, streetViewMetadata } from '..';
+import { latLngLiteralSchema } from '../generated/schema/latLngLiteralSchema';
 import { apiKey } from './consts';
 
 describe('streetView', () => {
@@ -14,5 +16,31 @@ describe('streetView', () => {
 
         // FIXME: Handle non-JSON results.
         expect(result).toBeFalsy();
+    });
+});
+
+describe('streetViewMetadata', () => {
+    test('OK', async () => {
+        const result = await streetViewMetadata({
+            key: apiKey,
+            location: '35.70434411172008,139.8114161291331',
+            heading: 0,
+            pitch: 20,
+            size: '600x400',
+        });
+
+        const expected = z
+            .object({
+                copyright: z.string(),
+                date: z.string(),
+                location: latLngLiteralSchema.strict(),
+                pano_id: z.string(),
+                status: z.literal('OK'),
+            })
+            .strict();
+
+        expect(() => {
+            expected.parse(result);
+        }).not.toThrow();
     });
 });
